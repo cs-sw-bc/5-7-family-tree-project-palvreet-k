74f173c5c64432402984c5cf16d4914f2745ecc2
palvreet-k <kaurpalvreet@gmail.com>
2026-02-26 21:37:34 -0500

    update

--- familyTree.test.js
+++ familyTree.test.js
@@ -11,14 +11,22 @@ const {
 } = require('./familyTree_new');
 
 const tree = simpsonFamily;
+console.log('Initial Simpson family tree:');
 console.log(tree);
-addChild(tree, 'Homer Simpson', 'New Child', '2000-01-01');
+// addChild(tree, 'Homer Simpson', 'New Child', '2000-01-01');
+console.log('Tree after attempting to add child to Homer:');
 console.log(tree);
 addChild(tree, 'Bart Simpson', 'Grandchild', '2020-01-01');
+console.log('Tree after adding grandchild to Bart Simpson:');
 console.log(tree);
 updatePerson(tree, 'Homer Simpson', { birthdate: '1956-05-12' });
+console.log('Tree after updating Homer Simpson birthdate:');
 console.log(tree);
-getDescendantsIterative(tree, 'Homer Simpson');
-getDescendantsRecursive(tree, 'Homer Simpson');
-getDescendantsIterative(tree, 'Bart Simpson');
-getDescendantsRecursive(tree, 'Bart Simpson');
+console.log('Descendants of Homer Simpson (iterative):');
+console.log(getDescendantsIterative(tree, 'Homer Simpson'));
+console.log('Descendants of Homer Simpson (recursive):');
+console.log(getDescendantsRecursive(tree, 'Homer Simpson'));
+console.log('Descendants of Bart Simpson (iterative):');
+console.log(getDescendantsIterative(tree, 'Bart Simpson'));
+console.log('Descendants of Bart Simpson (recursive):');
+console.log(getDescendantsRecursive(tree, 'Bart Simpson'));
